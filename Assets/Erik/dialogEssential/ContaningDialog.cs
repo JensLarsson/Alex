0da44b549b0ee2c64389ad8bf708350f0d523feb
PretentiousPanda <42602418+PretentiousPanda@users.noreply.github.com>
2019-02-07 14:34:40 +0100

    Erik implemented

---
+++ Assets/Erik/dialogEssential/ContaningDialog.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+[System.Serializable]
+public class ActivateDialog
+{
+    public bool onCollision;
+    public bool onCollisionStayWithDelay;
+    public float delay;
+    public bool onCollisionAndKeyDown;
+    [Tooltip("ifall ni vill starta dialogen vid ex man går höger använd scriptet 'interactOnKeyPress' ")]
+    public bool onFunctionCall;
+}
+public class ContaningDialog : MonoBehaviour
+{
+    [SerializeField] ActivateDialog activateDialogWith;
+    [SerializeField] List<Dialogs> newDialog = new List<Dialogs>();
+    [SerializeField] bool canRepeatTheDialog = false;
+    [HideInInspector] public bool canBeActivated = true;
+    public bool hasBeenRead = false;
+    [HideInInspector] public List<GameObject> siblings = new List<GameObject>();
+    bool isInDialogueTrigger = false;
+    float delay;
+
+
+    void Start()
+    {
+        delay = activateDialogWith.delay;
+    }
+    void OnDestroy()
+    {
+        for (int x = 0; x < siblings.Count; x++)
+        {
+            Destroy(siblings[x].gameObject);
+        }
+    }
+    public void startConversation()
+    {
+        if (activateDialogWith.onFunctionCall && activateDialogWith.delay < 0)
+        {
+            DialogManager.Instance.queNewDialog(newDialog, gameObject);
+            exitDialogue();
+        }
+        else if (!activateDialogWith.onFunctionCall)
+        {
+            DialogManager.Instance.queNewDialog(newDialog, gameObject);
+            exitDialogue();
+        }
+    }
+    void exitDialogue()
+    {
+        if (canRepeatTheDialog)
+        {
+            Vector3 pos = transform.position;
+            Transform parent = transform.parent.transform;
+
+            GameObject newDialogue = Instantiate(gameObject);
+            newDialogue.transform.SetParent(parent);
+            newDialogue.transform.position = pos;
+            newDialogue.name = gameObject.name;
+            newDialogue.GetComponent<ContaningDialog>().activateDialogWith.delay = delay;
+
+            newDialogue.GetComponent<ContaningDialog>().canBeActivated = false;
+
+
+            Destroy(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    void activateDialoge()
+    {
+
+        if (canBeActivated)
+            startConversation();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInDialogueTrigger)
+        {
+            if (!DialogManager.Instance.isInDialogue)
+            {
+                activateDialogWith.delay -= Time.deltaTime;
+                if (activateDialogWith.delay < 0)
+                {
+                    if (activateDialogWith.onCollisionStayWithDelay)
+                    {
+                        activateDialogWith.delay = delay;
+                        startConversation();
+                    }
+                }
+            }
+        }
+
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        isInDialogueTrigger = true;
+        if (canBeActivated)
+        {
+            if (activateDialogWith.onCollision)
+            {
+                if (col.tag == "Player")
+                {
+                    startConversation();
+                }
+            }
+        }
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+        
+        if (col.tag == "Player")
+        {
+            if (activateDialogWith.onCollisionAndKeyDown)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    startConversation();
+                }
+            }
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            isInDialogueTrigger = false;
+            canBeActivated = true;
+        }
+    }
+}
