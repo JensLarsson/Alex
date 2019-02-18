015487b1778ea8739c6b54d714e0a70384f9eb1c
PretentiousPanda <42602418+PretentiousPanda@users.noreply.github.com>
2019-02-18 01:00:46 +0100

    Snooke alpha

---
+++ Assets/Jens/MiniGame/RegisterPosition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegisterPosition : MonoBehaviour
+{
+
+    private void OnEnable()
+    {
+        TailManager.positionOccupation.Add(this.gameObject);
+    }
+
+
+    private void OnDisable()
+    {
+        //for (int i = TailManager.positionOccupation.Count - 1; i >= 0; i--)
+        for (int i = 0; i < TailManager.positionOccupation.Count; i++)
+        {
+            if (TailManager.positionOccupation[i] == this.gameObject)
+            {
+                Debug.Log("Derp" + i);
+                TailManager.positionOccupation.RemoveAt(i);
+                break;
+            }
+        }
+    }
+}
---
+++ Assets/Jens/MiniGame/TailManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TailManager : MonoBehaviour
+{
+    public GameObject blockPrefab;
+    public float tickTimer = 1.0f;
+    public static List<GameObject> positionOccupation = new List<GameObject>();
+
+    public List<GameObject> tailPart = new List<GameObject>();
+    public Vector2 startDirection = Vector2.up;
+    Vector3 dir;
+
+    private void Start()
+    {
+        dir = startDirection;
+        StartCoroutine(tick());
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            dir = Vector2.up;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            dir = Vector2.left;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            dir = Vector2.down;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            dir = Vector2.right;
+        }
+    }
+
+    IEnumerator tick()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(tickTimer);
+            Vector3 pos = tailPart[0].transform.position + dir;
+
+            bool ate = false;
+
+            for (int i = positionOccupation.Count - 1; i >= 0; i--)
+            {
+                if (positionOccupation[i].transform.position == pos)
+                {
+                    switch (positionOccupation[i].tag)
+                    {
+                        case "Apple":
+                            ate = true;
+                            Destroy(positionOccupation[i]);
+                            break;
+                    }
+                }
+            }
+            if (ate)
+            {
+                GameObject Tail = Instantiate(blockPrefab, pos, Quaternion.identity);
+                tailPart.Insert(0, Tail);
+            }
+            else
+            {
+
+                tailPart[tailPart.Count - 1].transform.position = pos;
+                tailPart.Insert(0, tailPart[tailPart.Count - 1]);
+                tailPart.RemoveAt(tailPart.Count - 1);
+            }
+        }
+    }
+}
