34f6edf378be3249e0586835027905ed4bedf0b4
PretentiousPanda <42602418+PretentiousPanda@users.noreply.github.com>
2019-02-19 13:00:30 +0100

    Derp

--- Assets/Dennis/MoveOnCollision.cs
+++ Assets/Dennis/MoveOnCollision.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MoveOnCollision : MonoBehaviour
 {
     private Vector2 startPos;
-    public float timeToDestination = 10f;
+    public float timeToDestination = 1.0f;
     private float timeToLerp;
     private Vector2 differenceInPos;
     private Vector2 travelPos;
@@ -17,38 +17,45 @@ public class MoveOnCollision : MonoBehaviour
     {
         differenceInPos = transform.position;
         travelPos = transform.position;
+        startPos = transform.position;
     }
     private void Update()
     {
-        startPos = transform.position;
-        timeToLerp += Time.deltaTime / timeToDestination;
+        //startPos = transform.position;
+        timeToLerp += Time.deltaTime;
         transform.position = Vector2.Lerp(startPos, travelPos, timeToLerp);
-        if(Vector2.Distance(startPos, travelPos) < narmeVarde)
+        if (Vector2.Distance(transform.position, travelPos) < narmeVarde && isLerping)
         {
             isLerping = false;
+            transform.position = new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y));
+            travelPos = transform.position;
+            startPos = transform.position;
+            timeToLerp = 0;
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (isLerping == false)
         {
+            startPos = transform.position;
+            timeToLerp = 0;
             if (collision.transform.tag == "Player")
             {
                 differenceInPos = -collision.transform.position + transform.position;
                 if (Mathf.Abs(differenceInPos.x) > Mathf.Abs(differenceInPos.y))
                 {
                     differenceInPos.y = 0;
-                    travelPos = new Vector2(transform.position.x + differenceInPos.x, transform.position.y);
-                    transform.position = new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y));
+
+                    travelPos = new Vector2(transform.position.x + differenceInPos.x / Mathf.Abs(differenceInPos.x), transform.position.y);
+
                 }
                 else
                 {
                     differenceInPos.x = 0;
-                    travelPos = new Vector2(transform.position.x, transform.position.y + differenceInPos.y);
-                    transform.position = new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y));
+                    travelPos = new Vector2(transform.position.x, transform.position.y + differenceInPos.y / Mathf.Abs(differenceInPos.y));
                 }
             }
-            
+
         }
         isLerping = true;
     }
