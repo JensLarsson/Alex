d685641e503d83a176862242f59c0d3decc661a4
Madaty <47196780+Madaty@users.noreply.github.com>
2019-02-26 16:52:20 +0100

    Add files via upload

---
+++ Dennis/Command.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public abstract class Command
+{
+    public abstract void Execute();
+
+}
+
+public class ActionCommand : Command
+{
+    private Action action;
+    public ActionCommand(Action OnExecute)
+    {
+        this.action = OnExecute;
+    }
+
+    public override void Execute()
+    {
+        action();
+    }
+}
