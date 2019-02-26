04d3f781d2b8bb0dfd251dab9bc649760b108fea
BackemackeSWE <47009098+BackemackeSWE@users.noreply.github.com>
2019-02-26 19:09:24 +0100

    animation done

--- Assets/Dennis/AnimationHandler.cs
+++ Assets/Dennis/AnimationHandler.cs
@@ -2,9 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationHandler : MonoBehaviour {
+public class AnimationHandler : MonoBehaviour
+{
     Animator animator;
     Rigidbody2D rb2d;
+    private int horizontal = 0;
+    private int vertical = 0;
+
+    private int horizontalAnim;
+    private int verticalAnim;
+
+    int previousX;
+    int previousY;
 
     private void Start()
     {
@@ -13,7 +22,49 @@ public class AnimationHandler : MonoBehaviour {
     }
     private void Update()
     {
-        animator.SetFloat("VelocityY", rb2d.velocity.y);
-        animator.SetFloat("VelocityX", rb2d.velocity.x);
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+        {
+            vertical = (int)verticalInput;
+
+            if (Mathf.Abs(previousX) > 0)
+            {
+                verticalAnim = 0;
+            }
+            else
+            {
+                verticalAnim = (int)verticalInput;
+            }
+        }
+        else
+        {
+            vertical = 0;
+            verticalAnim = 0;
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            if (Mathf.Abs(previousY) > 0)
+            {
+                horizontalAnim = 0;
+            }
+            else
+            {
+                horizontalAnim = (int)horizontalInput;
+            }
+        }
+        else
+        {
+            horizontal = 0;
+            horizontalAnim = 0;
+        }
+
+        previousX = horizontalAnim;
+        previousY = verticalAnim;
+
+        animator.SetFloat("VelocityY", verticalAnim);
+        animator.SetFloat("VelocityX", horizontalAnim);
     }
 }
