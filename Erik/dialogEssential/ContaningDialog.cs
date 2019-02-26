c622158b008c80c2a029f74e6bdf2f4defe79d0e
Madaty <47196780+Madaty@users.noreply.github.com>
2019-02-26 16:53:52 +0100

    Add files via upload

---
+++ Erik/dialogEssential/ContaningDialog.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.Events;
+using UnityEditor;
+
+
+[System.Serializable]
+public class ActivateDialog
+{
+    public bool onCollisionEnter;
+    public bool onCollisionStayWithDelay;
+    public float delay;
+    public bool onCollisionAndKeyDown;
+    [Tooltip("ifall ni vill starta dialogen vid ex man går höger använd scriptet 'interactOnKeyPress' ")]
+    public bool onFunctionCall;
+}
+
+public class ContaningDialog : MonoBehaviour
+{
+    [Header("Quests")]
+    public List<QuestSO> questsCheck;
+    public bool checkIfCompleted;
+	[SerializeField] string onChoseText;
+	[SerializeField] ActivateDialog activateDialogWith;
+
+	[Tooltip("om inget ljud finns kommer det helt enkelt inte spelas upp något")]
+	[SerializeField] AudioClip[] diffrentStartSounds;
+	[SerializeField] float startSoundPitchRange;
+
+
+	[SerializeField] List<Dialogs> newDialog = new List<Dialogs>();
+	[SerializeField] bool canRepeatTheDialog = false;
+	[HideInInspector] public bool canBeActivated = true;
+	 public bool hasBeenRead = false;
+	[HideInInspector] public List<GameObject> siblings = new List<GameObject>();
+	[SerializeField] GameObject[] answers;
+	[SerializeField] UnityEvent doAfterDialgue;
+	bool isInDialogueTrigger = false;
+    //start delay is the delay from active dialogue at the first frame (a reset)
+	float StartDelay;
+	float soundDelay;
+    [HideInInspector] public bool canPlaySound = true;
+
+
+
+    void Start()
+    {
+        StartDelay = activateDialogWith.delay;
+    }
+    
+    void OnDestroy()
+    {
+        for (int x = 0; x < siblings.Count; x++)
+        {
+            Destroy(siblings[x].gameObject);
+        }
+    }
+    public void startConversation()
+    {
+        if (activateDialogWith.onFunctionCall && activateDialogWith.delay < 0 || !activateDialogWith.onFunctionCall)
+        {
+            if (!DialogManager.Instance.isInDialogue)
+            {
+                //startDialogueSounds();
+                DialogManager.Instance.queNewDialog(diffrentStartSounds, startSoundPitchRange, newDialog, answers, doAfterDialgue, onChoseText, this.gameObject, hasBeenRead);
+            }
+        }
+    }
+    public void resetDialogue(bool chosedAnother)
+    {
+        //respawn the dialogue if the player picked another answer
+        //or if the dialogue can be played multiply times
+        if (chosedAnother || canRepeatTheDialog)
+        {
+            Vector3 pos = transform.position;
+
+            GameObject newDialogue = Instantiate(gameObject);
+
+            newDialogue.transform.position = pos;
+            newDialogue.name = gameObject.name;
+            newDialogue.GetComponent<ContaningDialog>().activateDialogWith.delay = StartDelay;
+
+            if (DialogManager.Instance.activeDialog.hasBeenRead)
+            {
+                newDialogue.GetComponent<ContaningDialog>().hasBeenRead = true;
+            }
+
+            if (transform.parent != null)
+            {
+                Transform parent = transform.parent.transform;
+                newDialogue.transform.SetParent(parent);
+            }
+            if (activateDialogWith.onCollisionEnter)
+            {
+                newDialogue.GetComponent<ContaningDialog>().canBeActivated = false;
+            }
+        }
+        Destroy(gameObject);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInDialogueTrigger)
+        {
+            if (activateDialogWith.onCollisionAndKeyDown)
+            {
+                if (Input.GetButtonDown("Submit"))
+                {
+                    startConversation();
+                }
+            }
+
+            if (DialogManager.Instance.activeDialog != null)
+            {
+                activateDialogWith.delay -= Time.deltaTime;
+                if (activateDialogWith.delay < 0)
+                {
+                    if (activateDialogWith.onCollisionStayWithDelay)
+                    {
+                        activateDialogWith.delay = StartDelay;
+                        startConversation();
+                    }
+                }
+            }
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        isInDialogueTrigger = true;
+        if (canBeActivated)
+        {
+            if (activateDialogWith.onCollisionEnter)
+            {
+                if (col.tag == "Player")
+                {
+						startConversation();
+                }
+            }
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            isInDialogueTrigger = false;
+            canBeActivated = true;
+        }
+    }
+}
---
+++ Erik/keyQuest/keyQuestMain.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyQuestMain : MonoBehaviour
+{
+    private static keyQuestMain instance;
+    public static keyQuestMain Instance { get { return instance; } }
+
+    int currentDoor = 0;
+    [Tooltip("Det är viktigt att dörrarna inte heter samma")]
+    public string[] nameOfAllDoors;
+    
+
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.LogError("There is too many keyQuestMain placed on scene");
+        }
+    }
+        // Update is called once per frame
+        void Update () {
+		
+	}
+    public void updateKeyQuest(int theDoor)
+    {
+        if (currentDoor < nameOfAllDoors.Length)
+        {
+            GameObject door = GameObject.Find(nameOfAllDoors[currentDoor]);
+            door.GetComponent<SceneTrigger>().unlock();
+
+            if (theDoor == currentDoor)
+            {
+                currentDoor++;
+            }
+            else
+            {
+                currentDoor = 0;
+            }
+            
+        }
+        if (currentDoor == nameOfAllDoors.Length)
+        {
+            QuestManager.Instance.addToCompletedQuests("Door");
+        }
+    }
+}
