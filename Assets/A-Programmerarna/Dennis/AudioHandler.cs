20ffd547167d48f755db36e4aefc0bf92f6a6ffe
BackemackeSWE <47009098+BackemackeSWE@users.noreply.github.com>
2019-03-05 17:55:55 +0100

    fotsteg audio

--- Assets/A-Programmerarna/Dennis/AudioHandler.cs
+++ Assets/A-Programmerarna/Dennis/AudioHandler.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class AudioHandler : MonoBehaviour {
 
+    [SerializeField]
+    private AudioClip[] footSteps;
+
+    private Rigidbody2D rb2d;
+    private AudioSource audioSource;
+
 	// Use this for initialization
 	void Start () {
-		
+        rb2d = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        PlayFootsteps();
 	}
+
+    private void PlayFootsteps()
+    {
+        if (rb2d.velocity.x != 0 || rb2d.velocity.y != 0)
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.clip = footSteps[Random.Range(0, footSteps.Length)];
+                audioSource.Play();
+            }
+        }
+    }
 }
