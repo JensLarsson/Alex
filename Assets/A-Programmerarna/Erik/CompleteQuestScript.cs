52c8a0044580b82e9582f8fef5a6f9c5237f5534
Ekkara <47211128+Ekkara@users.noreply.github.com>
2019-03-05 12:35:42 +0100

    kan nu spela en dialog sedan en annan som loopas

---
+++ Assets/A-Programmerarna/Erik/CompleteQuestScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompleteQuestScript : MonoBehaviour {
+    public QuestSO[] test;
+
+    public void completeQuest()
+    {
+        for (int i = 0; i < test.Length; i++)
+        {
+            QuestManager.Instance.addToCompletedQuests(test[i]);
+        }
+    }
+}
