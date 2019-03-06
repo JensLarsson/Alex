30243760b96c684db0621fdedf00a766a9fbfe3b
Madaty <47196780+Madaty@users.noreply.github.com>
2019-03-06 13:32:27 +0100

    Cat Puzzle

---
+++ Assets/A-Programmerarna/Adam/CatPuzzle/Teleporter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Teleporter : MonoBehaviour {
+
+    public GameObject outTeleporter;
+
+    void Start()
+    {
+        GetComponent<Collider2D>().isTrigger = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        other.transform.position = outTeleporter.transform.position;
+        if (other.tag == "Cat")
+        {
+            other.GetComponent<CatAI>().MakePathRequest(other.transform.position, other.transform.position, other.GetComponent<CatAI>().speed);
+        }
+    }
+}
