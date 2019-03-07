54ff77a62193f96731d35e1034a5c861e6cc2a98
PretentiousPanda <42602418+PretentiousPanda@users.noreply.github.com>
2019-03-07 15:24:02 +0100

    asdf

--- Assets/Script/BranchingDialogMovementController.cs
+++ Assets/Script/BranchingDialogMovementController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 using UnityEngine.Events;
 public class BranchingDialogMovementController : MonoBehaviour
 {
-
-    public float standStillActionTime = 10.0f;
+    [Tooltip("0 for deactivating Stand Still Action")]
+    public float standStillActionTime = 0.0f;
     public UnityEvent walkLeft, walkRight, walkUp, walkDown, standStill;
     Timer timer = new Timer();
 
@@ -16,18 +16,17 @@ public class BranchingDialogMovementController : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetButtonDown("Up") && walkUp != null && PlayerMovement.canMove) { invokeAction(walkUp); }
-        if (Input.GetButtonDown("down") && walkDown != null && PlayerMovement.canMove) { invokeAction(walkDown); }
-        if (Input.GetButtonDown("Left") && walkLeft != null && PlayerMovement.canMove) { invokeAction(walkLeft); }
-        if (Input.GetButtonDown("Right") && walkRight != null && PlayerMovement.canMove) { invokeAction(walkRight); }
+        if (Input.GetKeyDown(KeyCode.W) && walkUp != null && PlayerMovement.canMove) { invokeAction(walkUp); }
+        if (Input.GetKeyDown(KeyCode.S) && walkDown != null && PlayerMovement.canMove) { invokeAction(walkDown); }
+        if (Input.GetKeyDown(KeyCode.A) && walkLeft != null && PlayerMovement.canMove) { invokeAction(walkLeft); }
+        if (Input.GetKeyDown(KeyCode.D) && walkRight != null && PlayerMovement.canMove) { invokeAction(walkRight); }
         timer.Time += Time.deltaTime;
-        if (timer.expired) { invokeAction(standStill); }
+        if (timer.Duration > 0 && timer.expired) { invokeAction(standStill); }
     }
 
     void invokeAction(UnityEvent even)
     {
         even.Invoke();
-        Destroy(this.gameObject);
     }
 
 }
