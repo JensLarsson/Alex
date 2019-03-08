5aa7cf710d930a7e97f150969e385e234f95e4e7
PretentiousPanda <42602418+PretentiousPanda@users.noreply.github.com>
2019-03-08 15:49:38 +0100

    asd

--- Assets/Script/WallAlphaHole.cs
+++ Assets/Script/WallAlphaHole.cs
@@ -2,13 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WallAlphaHole : MonoBehaviour {
+public class WallAlphaHole : MonoBehaviour
+{
 
     public Material mat;
-    public Transform tran;
+    Transform tran;
+    private void OnLevelWasLoaded(int level)
+    {
+        tran = PlayerTracker.Instance.gameObject.transform;
+    }
 
-    void Update () {
+    private void Start()
+    {
+        tran = PlayerTracker.Instance.gameObject.transform;
+    }
+
+    void Update()
+    {
         mat.SetFloat("_MaskPosX", tran.position.x);
         mat.SetFloat("_MaskPosY", tran.position.y);
-	}
+    }
 }
