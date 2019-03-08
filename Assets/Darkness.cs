fd2872c5dfe2e590ee3338d2eacc8eb0e37129df
PretentiousPanda <42602418+PretentiousPanda@users.noreply.github.com>
2019-03-08 19:34:48 +0100

    asd

---
+++ Assets/Darkness.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Darkness : MonoBehaviour
+{
+    Transform pos;
+    void Start()
+    {
+        pos = PlayerTracker.Instance.gameObject.transform;
+        DontDestroyOnLoad(gameObject);
+    }
+    private void OnLevelWasLoaded(int level)
+    {
+        pos = PlayerTracker.Instance.gameObject.transform;
+    }
+}
