80648f4b3636cebcaa9a36ae82d6baf777886747
PretentiousPanda <42602418+PretentiousPanda@users.noreply.github.com>
2019-03-08 11:45:08 +0100

    asd

---
+++ Assets/Script/Item and Inventory/RemovefromInventory.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemovefromInventory : MonoBehaviour
+{
+    public Item item;
+
+    public void removeFromInventory()
+    {
+        Inventory.instance.removeItem(item);
+    }
+}
