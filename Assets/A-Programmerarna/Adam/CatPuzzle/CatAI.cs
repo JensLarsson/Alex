7ecf4d9cfad507773ad3f9aa7af5312d702d3b05
PretentiousPanda <42602418+PretentiousPanda@users.noreply.github.com>
2019-03-08 17:12:19 +0100

    nlsefkjdhlgksjd

--- Assets/A-Programmerarna/Adam/CatPuzzle/CatAI.cs
+++ Assets/A-Programmerarna/Adam/CatPuzzle/CatAI.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Collider2D))]
-public class CatAI : MonoBehaviour {
+public class CatAI : MonoBehaviour
+{
 
     public bool displayPath;
     public float speed;
@@ -17,6 +18,7 @@ public class CatAI : MonoBehaviour {
     Animator anim;
     public GameObject player;
     public GameObject cage;
+    public GameObject lightVision;
     public LayerMask visionMask;
 
     public Vector3 moveDirection;
@@ -31,14 +33,16 @@ public class CatAI : MonoBehaviour {
     }
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         anim = GetComponent<Animator>();
         anim.SetInteger("moveDirectionY", 1);
         rb2D = GetComponent<Rigidbody2D>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         anim.SetFloat("distanceToPlayer", Vector3.Distance(player.transform.position, transform.position));
         Vector3 direction = player.transform.position - transform.position;
         direction = NormalizeDirections(direction.normalized);
@@ -46,6 +50,18 @@ public class CatAI : MonoBehaviour {
 
         RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(anim.GetInteger("moveDirectionX"), anim.GetInteger("moveDirectionY")), Mathf.Infinity, visionMask);
         Debug.DrawRay(transform.position, new Vector2(anim.GetInteger("moveDirectionX") * 20, anim.GetInteger("moveDirectionY") * 20), Color.red);
+
+
+        if (anim.GetInteger("moveDirectionX") != 0)
+        {
+            lightVision.transform.position = new Vector3(transform.position.x + 10 * anim.GetInteger("moveDirectionX"), transform.position.y);
+            lightVision.transform.localScale = new Vector3(20, 0.5f, 1);
+        }
+        else if (anim.GetInteger("moveDirectionY") != 0)
+        {
+            lightVision.transform.position = new Vector3(transform.position.x, transform.position.y + 10 * anim.GetInteger("moveDirectionY"));
+            lightVision.transform.localScale = new Vector3(0.5f, 20, 1);
+        }
         if (hit)
         {
             Debug.Log("Hit Something");
@@ -87,65 +103,6 @@ public class CatAI : MonoBehaviour {
             }
         }
         return temp;
-    }
-
-    public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
-    {
-        if (pathSuccessful)
-        {
-            onPath = true;
-            path = newPath;
-            targetIndex = 0;
-            StopCoroutine("FollowPath");
-            StartCoroutine("FollowPath");
-        }
-    }
-
-    IEnumerator FollowPath()
-    {
-        Vector3 currentWaypoint = transform.position;
-        if (path.Length > 0)
-        {
-            currentWaypoint = path[0];
-        }
-        while (true)
-        {
-            if (transform.position == currentWaypoint)
-            {
-                targetIndex++;
-                if (targetIndex >= path.Length)
-                {
-                    onPath = false;
-                    yield break;
-                }
-                currentWaypoint = path[targetIndex];
-            }
-
-            transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
-            yield return null;
-
-        }
-    }
-
-    public void OnDrawGizmos()
-    {
-        if (path != null && displayPath)
-        {
-            for (int i = targetIndex; i < path.Length; i++)
-            {
-                Gizmos.color = Color.black;
-                Gizmos.DrawCube(path[i], new Vector3(0.2f, 0.2f));
-
-                if (i == targetIndex)
-                {
-                    Gizmos.DrawLine(transform.position, path[i]);
-                }
-                else
-                {
-                    Gizmos.DrawLine(path[i - 1], path[i]);
-                }
-            }
-        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
--- Assets/A-Programmerarna/Adam/CatPuzzle/Flee.cs
+++ Assets/A-Programmerarna/Adam/CatPuzzle/Flee.cs
@@ -11,7 +11,6 @@ public class Flee : CatBase
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
         base.OnStateEnter(animator, stateInfo, layerIndex);
-        //cat.GetComponent<CatAI>().MakePathRequest(cat.transform.position, cat.transform.position, catAI.speed);
         direction = catAI.moveDirection;
         animator.SetInteger("moveDirectionX", (int)direction.x);
         animator.SetInteger("moveDirectionY", (int)direction.y);
@@ -40,19 +39,7 @@ public class Flee : CatBase
             animator.SetInteger("moveDirectionY", (int)direction.y);
         }
         direction = new Vector3(animator.GetInteger("moveDirectionX"), animator.GetInteger("moveDirectionY"));
-        //cat.transform.Translate(new Vector3(Time.deltaTime * catAI.speed * direction.x, Time.deltaTime * catAI.speed * direction.y, 0));
         catAI.rb2D.velocity = new Vector2(catAI.speed * direction.x, catAI.speed * direction.y);
-
-
-        /*if (Vector3.Distance(target, cage.transform.position) < 0.5f)
-        {
-            target = cage.transform.position;
-        }
-        else
-        {
-            target = new Vector3(-direction.x * 2.0f, -direction.y * 2.0f);
-        }
-        cat.GetComponent<CatAI>().MakePathRequest(cat.transform.position, target, speed);*/
     }
 
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
@@ -69,38 +56,4 @@ public class Flee : CatBase
     //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
     //
     //}
-
-    Vector3 NormalizeDirections(Vector3 _direction)
-    {
-        Vector3 temp = new Vector3();
-        if (_direction.x < 0.5f && _direction.x > -0.5f)
-        {
-            temp.x = 0;
-        }
-        else if (_direction.x >= 0.5f)
-        {
-            temp.x = 1;
-        }
-        else if (_direction.x <= -0.5f)
-        {
-            temp.x = -1;
-        }
-
-        if (temp.x == 0)
-        {
-            if (_direction.y < 0.5f && _direction.y > -0.5f)
-            {
-                temp.y = 0;
-            }
-            else if (_direction.y >= 0.5f)
-            {
-                temp.y = 1;
-            }
-            else if (_direction.y <= -0.5f)
-            {
-                temp.y = -1;
-            }
-        }
-        return temp;
-    }
 }
