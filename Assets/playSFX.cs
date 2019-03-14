c558dc2227ec816e7cf2b1d7d73e26951d1c8f9e
PretentiousPanda <42602418+PretentiousPanda@users.noreply.github.com>
2019-03-14 22:39:51 +0100

    asd

---
+++ Assets/playSFX.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playSFX : MonoBehaviour
+{
+
+    public AudioClip clip;
+
+    public void playClip()
+    {
+        AudioManager.instance.playSFXClip(clip);
+    }
+}
