742dec9e5c4603d74efad86fcb2985b0f671b0ca
PretentiousPanda <42602418+PretentiousPanda@users.noreply.github.com>
2019-03-18 14:17:08 +0100

    asd

---
+++ Assets/SpawnRate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRate : MonoBehaviour
+{
+
+    [Range(0.0f, 1.0f)] public float spawnRate = 0.5f;
+    public GameObject[] children = new GameObject[2];
+    private void Start()
+    {
+        if (spawnRate > Random.Range(0.0f, 0.99f))
+        {
+            foreach (GameObject gObject in children)
+            {
+                Debug.Log("Derp");
+                gObject.SetActive(true);
+            }
+        }
+    }
+
+}
