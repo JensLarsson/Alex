d73cb56ef0f813d16965c7bbb1b4583c0e97e9e7
BackemackeSWE <47009098+BackemackeSWE@users.noreply.github.com>
2019-03-18 20:07:52 +0100

    prototyp emo puzzel

---
+++ Assets/A-Programmerarna/Dennis/PuzzelController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzelController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Puzzel;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (!Puzzel.activeSelf)
+                {
+                    Puzzel.SetActive(true);
+                    PlayerMovement.canMove = false;
+                }
+                else if (Puzzel.activeSelf)
+                {
+                    Puzzel.SetActive(false);
+                    PlayerMovement.canMove = true;
+                }
+            }
+        }
+    }
+}
