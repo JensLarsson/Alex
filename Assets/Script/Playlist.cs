c10ec8a48c4701a439da39ee139dd4040950f60d
PretentiousPanda <42602418+PretentiousPanda@users.noreply.github.com>
2019-03-18 10:54:25 +0100

    asd

--- Assets/Script/Playlist.cs
+++ Assets/Script/Playlist.cs
@@ -3,29 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
+public class conditionPlaylist
+{
+    public QuestSO completedQuest;
+    public List<AudioClip> songs = new List<AudioClip>();
+}
 
+public class Playlist : MonoBehaviour
+{
 
-    public class Playlist : MonoBehaviour {
-
-    public enum PlayBeaviour { loopLast, loopPlaylist};
+    public enum PlayBeaviour { loopLast, loopPlaylist };
     public PlayBeaviour playlistBehaviour = PlayBeaviour.loopLast;
 
     public List<AudioClip> songs = new List<AudioClip>();
+    public conditionPlaylist playlistWithCondition;
 
     void Start()
     {
-        if (songs.Count == 1)
+        if (playlistWithCondition == null)
         {
-            AudioManager.instance.changeSong(songs[0]);
-        }
-        else if (songs.Count > 0)
-        {
-            AudioManager.instance.playSongs(songs, playlistBehaviour);
-        }
-        else
-        {
-            Debug.LogError("No songs in playlist");
+            if (songs.Count == 1)
+            {
+                AudioManager.instance.changeSong(songs[0]);
+            }
+            else if (songs.Count > 0)
+            {
+                AudioManager.instance.playSongs(songs, playlistBehaviour);
+            }
+            else
+            {
+                Debug.LogError("No songs in playlist");
+            }
         }
     }
-	
+
 }
