a216f0fb25a8826c5d97b5e5547d69e334409168
PretentiousPanda <42602418+PretentiousPanda@users.noreply.github.com>
2019-03-21 21:30:57 +0100

    asd

---
+++ Assets/A-Programmerarna/Jens/Circle.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    const int size = 500;
+    Vector3[] outerCircePositions = new Vector3[size];
+    Vector3[] innerCirclePositions = new Vector3[size / 2];
+    public Material material;
+    public float radius = 5;
+    Color col = new Color(1, 0, 1);
+    // Use this for initialization
+    void Start()
+    {
+        float pi = Mathf.PI * 2;
+
+        float f = pi / outerCircePositions.Length;
+        for (int i = 0; i < outerCircePositions.Length; i++)
+        {
+            outerCircePositions[i].y = Mathf.Sin(f * i) * radius + 1.7f;
+            outerCircePositions[i].x = Mathf.Cos(f * i) * radius - 5.5f;
+        }
+
+        f = pi / innerCirclePositions.Length;
+        for (int i = 0; i < innerCirclePositions.Length; i++)
+        {
+            innerCirclePositions[i].y = Mathf.Sin(f * i) * radius / 2 + 1.7f;
+            innerCirclePositions[i].x = Mathf.Cos(f * i) * radius / 2 - 5.5f;
+        }
+    }
+
+
+
+    public void OnPostRender()
+    {
+        //q += Mathf.Sin(Time.time * 000000.1f);
+        for (int i = 0; i < size; i++)
+        {
+            int j = (int)(i * Time.time * 0.1f);
+            while (j >= size * 0.5f)
+            {
+                j -= (int)(size * 0.5f);
+            }
+            drawLines(outerCircePositions[i], innerCirclePositions[j]);
+        }
+
+    }
+
+    void drawLines(Vector3 pointA, Vector3 pointB)
+    {
+        GL.Begin(GL.LINES);
+        material.SetPass(0);
+        GL.Color(col);
+        GL.Vertex(pointA);
+        GL.Color(Color.cyan);
+        GL.Vertex(pointB);
+
+        GL.End();
+    }
+}
---
+++ Assets/A-Programmerarna/Jens/HueShift.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HueShift : MonoBehaviour
+{
+
+    SpriteRenderer sr;
+    public float speed = 1.0f;
+    // Use this for initialization
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float H, S, V;
+        Color.RGBToHSV(sr.color, out H, out S, out V);
+        H += Time.deltaTime * speed;
+        sr.color = Color.HSVToRGB(H, S, V);
+    }
+}
