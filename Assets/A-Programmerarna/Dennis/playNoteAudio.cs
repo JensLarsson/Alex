ee542cbec0c0ab70645bf4c6d88d54ed791723b1
PretentiousPanda <42602418+PretentiousPanda@users.noreply.github.com>
2019-03-22 17:57:31 +0100

    jkl

--- Assets/A-Programmerarna/Dennis/playNoteAudio.cs
+++ Assets/A-Programmerarna/Dennis/playNoteAudio.cs
@@ -12,6 +12,6 @@ public class playNoteAudio : MonoBehaviour
     
     public void PlayNoteAudio()
     {
-        AudioManager.instance.playSFXClip(NoteAudio, true);
+        AudioManager.instance.playSFXClip(NoteAudio);
     }
 }
